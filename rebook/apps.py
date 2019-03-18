8e27e3aa3c1002e857da1ad3e1cf223e0a3c21bd
Rita Cerdeira <ritacerdeira@ua.pt>
2019-03-18 15:51:34 +0000

    Created project

---
+++ rebook/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class RebookConfig(AppConfig):
+    name = 'rebook'
---
+++ venv/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/home/rita/Dropbox/LEI/TPW/re_book/venv/bin/python
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
