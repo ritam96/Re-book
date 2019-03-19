9a435d00d34d671ed77ac7256dfd7942e1e299af
Rita Cerdeira <ritacerdeira@ua.pt>
2019-03-19 12:15:54 +0000

    Database created with ORM models

--- rebook/admin.py
+++ rebook/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from rebook.models import *
 
 # Register your models here.
+admin.site.register(User)
+admin.site.register(Author)
+admin.site.register(Publisher)
+admin.site.register(Book)
+admin.site.register(BookState)
+admin.site.register(BookGoals)
+admin.site.register(BookInstance)
+admin.site.register(Proposal)
+admin.site.register(TradeState)
+admin.site.register(Trade)
+
--- rebook/models.py
+++ rebook/models.py
@@ -1,3 +1,59 @@
 from django.db import models
 
 # Create your models here.
+
+
+class User(models.Model):
+    name = models.CharField(max_length=70)
+    email = models.EmailField()
+    password = models.CharField(max_length=120)
+    address = models.CharField(max_length=200)
+    photo = models.ImageField()
+
+
+class Author(models.Model):
+    name = models.CharField(max_length=70)
+
+
+class Publisher(models.Model):
+    name = models.CharField(max_length=70)
+
+
+class Book(models.Model):
+    ISBN = models.CharField(max_length=20, primary_key=True)
+    title = models.CharField(max_length=100)
+    author = models.ForeignKey(Author, on_delete=models.CASCADE)
+    publisher = models.ForeignKey(Publisher, on_delete=models.CASCADE)
+    year = models.IntegerField()
+    cover = models.ImageField()
+
+
+class BookState(models.Model):
+    state = models.CharField(max_length=30)
+
+
+class BookGoals(models.Model):
+    goal = models.CharField(max_length=30)
+
+
+class BookInstance(models.Model):
+    ISBN = models.ForeignKey(Book, on_delete=models.PROTECT)
+    User = models.ForeignKey(User, on_delete=models.PROTECT)
+    state = models.ForeignKey(BookState, on_delete=models.PROTECT)
+    readingState = models.DecimalField(max_digits=4, decimal_places=3)
+    goal = models.ForeignKey(BookGoals, on_delete=models.PROTECT)
+
+
+class Proposal(models.Model):
+    bookInstance = models.ForeignKey(BookInstance, on_delete=models.CASCADE)
+    user2 = models.ForeignKey(User, on_delete=models.PROTECT)
+
+
+class TradeState(models.Model):
+    state = models.CharField(max_length=20)
+
+
+class Trade(models.Model):
+    proposalID = models.OneToOneField(Proposal, on_delete=models.CASCADE)
+    bookInstance2 = models.ForeignKey(BookInstance, on_delete=models.CASCADE)
+    state = models.ForeignKey(TradeState, on_delete=models.PROTECT)
