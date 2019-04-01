c2cd369ef70ef0e503cee52859bc29d51c776d4d
Rita Cerdeira <ritacerdeira@ua.pt>
2019-04-01 18:00:33 +0100

    Added static files. Interface commit

--- rebook/views.py
+++ rebook/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from django.http import HttpResponse, HttpRequest
+from datetime import datetime
+
 
 # Create your views here.
+def rebook(request):
+    return render(request, 'layout.html')
